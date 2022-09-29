0a30715c2f514e8aa0b37199c14e08ea3d71a858
SalomaoK <114115127+sekim-dev@users.noreply.github.com>
2022-09-29 16:09:40 -0300

    criado projeto

---
+++ backend-challenge/src/main/java/br/me/desafio/backendchallenge/userResource/Resources.java
@@ -0,0 +1,18 @@
+package br.me.desafio.backendchallenge.userResource;
+
+import br.me.desafio.backendchallenge.user.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class Resources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "salo","salo@salo", "1111111","1234");
+        return ResponseEntity.ok().body(u);
+    }
+}
